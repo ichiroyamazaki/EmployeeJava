7c117d8c0cb6ac1bee9b8ba9a27c0686432778b1
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-23 12:55:53 +0800

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,15 @@
+package Employee;
+
+public class Employee {
+    private String name;
+
+    // Setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter method for name
+    public String getName() {
+        return name;
+    }
+}
