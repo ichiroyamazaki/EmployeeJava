0fe54d973f22acb3b727cf84108dbae52c6189c2
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-23 12:56:58 +0800

    Create FullTimeEmployee.java

---
+++ FullTimeEmployee.java
@@ -0,0 +1,15 @@
+package Employee;
+
+public class FullTimeEmployee extends Employee {
+    private double monthlySalary;
+
+    // Setter method for monthlySalary
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    // Getter method for monthlySalary
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+}
